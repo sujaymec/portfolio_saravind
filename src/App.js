f5814b837bf80cf383c46a26598b980c1ff57395
Sujay S <sujaymec>
2024-06-05 00:26:14 +0530

    adding material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      This is an update
-    </div>
+    <>
+      <div className="sp-content hero-section sp-flex-column">
+        <h2>Hey I'm Saravind 👋</h2>
+        <p className='sp-text-para'>I'm a software designer with a deep passion for user experience and growth designs. Currently, Sr. Product designer at We Create Problems, an platform helps recruiters assess candidates more effectively. </p>
+        <p className='sp-text-para'>Previously, I designed key products for startups including itilte and Economize Cloud, among others. Before transitioning to design, I specialised in cloud technology operations across multiple firms.</p>
+      </div>
+    </>
   );
 }
 
