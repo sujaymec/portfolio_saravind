b20b0199209f25113ab601a3137f7241a5b08eb4
Sujay S <sujaymec>
2024-06-08 00:07:40 +0530

    adding logo file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="sp-content hero-section sp-flex-column">
         <h2>Hey I'm Saravind 👋</h2>
         <p className='sp-text-para'>I'm a software designer with a deep passion for user experience and growth designs. Currently, Sr. Product designer at We Create Problems, an platform helps recruiters assess candidates more effectively. </p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import logo_saravind from './logo_saravind.svg'
+
+function Navbar() {
+    return (
+      <>
+        <nav className="sp-content sp-flex-row">
+            <div className="logo">
+                <img src={logo_saravind} alt="" />
+            </div>
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>Resume</li>
+            </ul>
+        </nav>
+      </>
+    );
+  }
+  
+  export default Navbar;
