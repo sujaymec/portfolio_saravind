6496b4409160288bcac06c98d1760caa97e50257
Sujay S <sujaymec>
2024-06-09 22:22:47 +0530

    adding images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import separator from './components/separator_bar.svg'
+import projectThumbnail from './components/thumbnail_1.jpg'
 import Navbar from './components/Navbar';
 
 function App() {
@@ -9,6 +11,15 @@ function App() {
         <h2>Hey I'm Saravind 👋</h2>
         <p className='sp-text-para'>I'm a software designer with a deep passion for user experience and growth designs. Currently, Sr. Product designer at We Create Problems, an platform helps recruiters assess candidates more effectively. </p>
         <p className='sp-text-para'>Previously, I designed key products for startups including itilte and Economize Cloud, among others. Before transitioning to design, I specialised in cloud technology operations across multiple firms.</p>
+      </div>
+      <div className="sp-content sp-separator">
+        <img src={separator} alt="" />
+      </div>
+      <div className="sp-content sp-flex-column">
+        <h4 className="selected-work">
+          # Selected work
+        </h4>
+        <img src={projectThumbnail} alt="" className="project-thumbnail" />
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import logo_saravind from './logo_saravind.svg'
 function Navbar() {
     return (
       <>
-        <nav className="sp-content sp-flex-row">
+        <nav className="sp-content sp-flex-row sp-navbar">
             <div className="logo">
                 <img src={logo_saravind} alt="" />
             </div>
