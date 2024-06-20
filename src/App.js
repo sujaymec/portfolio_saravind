7e03a7fa985737435cefee2c79f3658d3bf0f25c
Sujay S <sujaymec>
2024-06-21 00:52:41 +0530

    penultimate changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import separator from './components/separator_bar.svg'
-import projectThumbnail_1 from './components/thumbnail_1.jpg'
-import projectThumbnail_2 from './components/img/thumbnail_2.jpg'
-import projectThumbnail_3 from './components/img/thumbnail_3.jpg'
+import projectThumbnail_1 from './components/img/thumbnail_1.png'
+// import projectThumbnail_2 from './components/img/thumbnail_2.jpg'
+// import projectThumbnail_3 from './components/img/thumbnail_3.jpg'
 import icon_briefcase from './components/img/icon_briefcase.svg'
 import icon_devices from './components/img/icon_devices.svg'
 import icon_steps from './components/img/icon_steps.svg'
@@ -10,6 +10,7 @@ import icon_social_linkedin from './components/img/icon_social_linkedin.svg'
 import icon_social_twitter from './components/img/icons_social_twitter.svg'
 import icon_social_behance from './components/img/icon_social_behance.svg'
 import icons_social_medium from './components/img/icons_social_medium.svg'
+import icon_mail from './components/img/icon_mail.svg'
 import Navbar from './components/Navbar';
 import ProjectSection from './components/ProjectSection';
 
@@ -30,19 +31,19 @@ function App() {
           # Selected work
       </h4>
       <ProjectSection thumbNail={projectThumbnail_1} icon_array ={icon_array} />
-      <ProjectSection thumbNail={projectThumbnail_2} icon_array ={icon_array} />
-      <ProjectSection thumbNail={projectThumbnail_3} icon_array ={icon_array}/>
+      <ProjectSection thumbNail={projectThumbnail_1} icon_array ={icon_array} />
+      <ProjectSection thumbNail={projectThumbnail_1} icon_array ={icon_array}/>
       <div className="sp-content sp-separator">
         <img src={separator} alt="" />
       </div>
       <h4 className="sp-content selected-work">
           # Contact
       </h4>
-      <br />
       <div className="sp-content">
         <p>I’m looking to collaborate on interesting projects with great people and teams. Drop an email and lets connect!</p>
         <div className="email-availability">
           <button>
+            <span><img src={icon_mail} alt="" /></span>
             Copy email
           </button>
         </div>
@@ -55,10 +56,10 @@ function App() {
       </h4>
       <div className="sp-content">
         <ul className='social-icons'>
-          <li><img src={icon_social_linkedin} alt='' /></li>
-          <li><img src={icon_social_twitter} alt='' /></li>
-          <li><img src={icon_social_behance} alt='' /></li>
-          <li><img src={icons_social_medium} alt='' /></li>
+          <li><a href="https://www.linkedin.com/in/saravindkv/"><img src={icon_social_linkedin} alt='' /></a></li>
+          <li><a href="https://www.linkedin.com/in/saravindkv/"><img src={icon_social_twitter} alt='' /></a></li>
+          <li><a href="https://www.linkedin.com/in/saravindkv/"><img src={icon_social_behance} alt='' /></a></li>
+          <li><a href="https://www.linkedin.com/in/saravindkv/"><img src={icons_social_medium} alt='' /></a></li>
         </ul>
       </div>
     </>
