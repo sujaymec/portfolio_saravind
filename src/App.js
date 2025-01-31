e873690f7fdbb54571b0929536f1d152859266da
sujaymec <sujaymec@gmail.com>
2025-01-31 14:04:11 +0530

    portfolio link change

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
       <Navbar />
       <div className="sp-content hero-section sp-flex-column">
         <h2>Hey I'm Saravind <img src={palm} alt="" /></h2>
-        <p>I'm a product designer with 3+ years of experience, passionate about user experience and growth designs. Currently, <span className='sp-semi-bold'>Sr. Product designer</span> at <span className='sp-semi-bold sp-underline'><a href='https://www.wecreateproblems.com/' target='_blank' rel="noreferrer">We Create Problems</a></span>, a platform that helps recruiters assess candidates more effectively. </p>
-        <p>Previously, I designed key products for startups including itilite and Economize Cloud, among others. Before transitioning to design, I specialised in cloud technology operations across multiple firms.</p>
+        <p>I'm a product designer with 4+ years of experience, passionate about user experience and growth designs. Currently, <span className='sp-semi-bold'>Sr. Product designer</span> at <span className='sp-semi-bold sp-underline'><a href='https://www.wecreateproblems.com/' target='_blank' rel="noreferrer">We Create Problems</a></span>, a platform that helps recruiters assess candidates more effectively. </p>
+        {/* <p>Previously, I designed key products for startups including itilite and Economize Cloud, among others. Before transitioning to design, I specialised in cloud technology operations across multiple firms.</p> */}
       </div>
       <div className="sp-content sp-separator">
         <img src={separator} alt="" />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
             <ul className="nav-menu">
                 <li className='active'>Home</li>
                 <li onClick={() => openInNewTab("https://saravindkv.notion.site/About-me-wip-71317a8b699f41f9909fd5e76e729815")}>About</li>
-                <li><a href="https://drive.google.com/file/d/1sLZfyCZfJj3JHWr-Q_lzxvH1LxEasF65/view?usp=sharing" target='_blank' rel="noreferrer">Resume</a></li>
+                <li><a href="https://drive.google.com/drive/folders/1lBJBAOxKAI0-cG4rLG477W5nE0CGqjRq?usp=drive_link" target='_blank' rel="noreferrer">Resume</a></li>
             </ul>
         </nav>
       </>
